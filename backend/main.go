8da4a8eb5c63176a28221b0559f4163adbdab532
Slumhunden <kroghbastin@gmail.com>
2024-09-15 13:19:24 +0200

    #3 Added the search API endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	//Internal imports
+	// Internal imports
 	"github.com/UpsDev42069/BM_Search_Engine/backend/handlers"
 	"net/http"
 
-	//External imports
+	// External imports
 	"github.com/gorilla/mux"
 )
 
-
-func main(){
+func main() {
 	r := mux.NewRouter()
+	// Existing routes
 	r.HandleFunc("/", handlers.RootGet).Methods("GET")
+
+	// New route for /api/search
+	r.HandleFunc("/api/search", handlers.SearchHandler).Methods("GET")
+
 	http.ListenAndServe(":8080", r)
 }
